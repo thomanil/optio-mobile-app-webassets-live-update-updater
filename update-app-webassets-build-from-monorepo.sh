2d2425dc0c7933b4579b994a58bb07783727e4d3
Thomas Kjeldahl Nilsson <thomas@tknilsson.com>
2024-11-07 15:10:31 +0100

    update script

--- update-app-webassets-build-from-monorepo.sh
+++ update-app-webassets-build-from-monorepo.sh
@@ -2,7 +2,7 @@ set -e
 
 SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
-echo "Building monorepo"
+echo "--Building monorepo mobile app web assets--"
 MONOREPO_CURRENT_GIT_SHA="$(git rev-parse HEAD)"
 cd ..
 cd Monorepo
@@ -10,18 +10,17 @@ cd apps
 ./scripts/clean-build-and-synch-app.sh production
 
 
-
-echo "Copying app web assets back to the updater repo"
+echo "--Copying app web assets back to the updater repo--"
 cd "$SCRIPT_DIR" # Back to this repo
 cp -r ../Monorepo/apps/staged-assets/* ./prebuilt-dist
 
 
 # First check if there are any changes in prebuilt-dist
 if git diff --quiet ./prebuilt-dist && git diff --cached --quiet ./prebuilt-dist; then
-    echo "no changes to built web assets since last commit, nothing to do. Aborting."
+    echo "--No changes to built web assets since last commit, nothing to do. Aborting.--"
     exit 1
 else
-    echo "Committing and pushing latest build"
+    echo "--Committing and pushing latest build--"
     COMMIT_COMMENT="Mobile app dist files built from monorepo commit ${MONOREPO_CURRENT_GIT_SHA}"
     git add ./prebuilt-dist
     git commit -m "$COMMIT_MSG"
