340c29f16596731607194c432932c98e9aaee8a7
Thomas Kjeldahl Nilsson <thomas@tknilsson.com>
2024-11-07 15:00:22 +0100

    Start on build script

---
+++ update-app-webassets-build-from-monorepo.sh
@@ -0,0 +1,19 @@
+set -e
+
+SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+echo "Building monorepo"
+MONOREPO_CURRENT_GIT_SHA="$(git rev-parse HEAD)"
+cd ..
+cd Monorepo
+cd apps
+./scripts/clean-build-and-synch-app.sh production
+
+
+
+echo "Copying app web assets back to the updater repo"
+cd "$SCRIPT_DIR" # Back to this repo
+cp -r ../Monorepo/apps/staged-assets/* ./prebuilt-dist
+
+
+
