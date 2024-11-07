b30c3f9086482a37281195fea568af3a21990542
Thomas Kjeldahl Nilsson <thomas@tknilsson.com>
2024-11-07 15:30:13 +0100

    More output

--- update-app-webassets-build-from-monorepo.sh
+++ update-app-webassets-build-from-monorepo.sh
@@ -30,6 +30,7 @@ else
     git commit -m "${COMMIT_COMMENT}"
     git push origin main
     echo "Done! Latest mobile app web asset build discoverable in Appflow, can now be distributed via Live Update there."
+    echo "Built based on: https://github.com/OptioIncentives/Monorepo/commit/${MONOREPO_CURRENT_GIT_SHA}"
 fi
 
 
